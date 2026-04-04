eef709289b075578e49cae1b09ef3917453ea873
Alzejones Dias <alzejones@gmail.com>
2026-04-04 15:16:41 -0300

    stripee

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
+import { StripeProvider } from '@stripe/stripe-react-native';
 import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
 import { ThemeProvider } from "../contexts/ThemeContext";
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <Slot />
-      </AuthProvider>
-    </ThemeProvider>
+    <StripeProvider publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY || ""}>
+      <ThemeProvider>
+        <AuthProvider>
+          <Slot />
+        </AuthProvider>
+      </ThemeProvider>
+    </StripeProvider>
   );
 }
+
