98ee6945625530df13c20ad3640356760a9c0c58
Alzejones Dias <alzejones@gmail.com>
2026-04-05 17:25:37 -0300

    stripe

--- supabase/functions/stripe-checkout/index.ts
+++ supabase/functions/stripe-checkout/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import Stripe from "https://esm.sh/stripe@14.14.0?target=deno"
+import Stripe from "npm:stripe@14.14.0"
 
 const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') ?? '', {
   apiVersion: '2023-10-16',
