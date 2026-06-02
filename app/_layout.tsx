0cd55e4fb5a779e50184d3e891f76b8407ba0f8a
Alzejones Dias <alzejones@gmail.com>
2026-06-01 23:46:34 -0300

    debug: add Slot to minimal layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,8 @@
-import { Text, View } from 'react-native';
+import { Slot } from 'expo-router';
+import { View, Text } from 'react-native';
 console.log('[DEBUG 1] _layout.tsx carregado');
 
 export default function RootLayout() {
   console.log('[DEBUG 2] componente renderizando');
-  return (
-    <View>
-      <Text>OK</Text>
-    </View>
-  );
+  return <Slot />;
 }
