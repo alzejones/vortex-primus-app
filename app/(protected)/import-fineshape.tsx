62426f377c6d6ee5a91b07e51d1d53ef3f84dacc
Alzejones Dias <alzejones@gmail.com>
2026-08-01 16:51:43 -0300

    fix: usar expo-file-system/legacy para evitar warning de readAsStringAsync deprecated

--- app/(protected)/import-fineshape.tsx
+++ app/(protected)/import-fineshape.tsx
@@ -1,7 +1,7 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 import * as DocumentPicker from "expo-document-picker";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import Papa from "papaparse";
 import React, { useState } from "react";
 import {
